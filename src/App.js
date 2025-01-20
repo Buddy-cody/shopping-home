dcaca7c3149602788f51d8347bc79a51ca28a661
Mujakkir2 <104411871+Mujakkir2@users.noreply.github.com>
2025-01-20 12:39:45 +0530

    Added filters functionalities

--- src/App.js
+++ src/App.js
@@ -3,39 +3,111 @@ import React, { useState, useEffect } from "react";
 function App() {
   const [categories, setCategories] = useState([]);
   const [products, setProducts] = useState([]);
+  const [search, setSearch] = useState("");
+  const [filterProducts, setFilterProducts] = useState([]);
 
   function LoadCategory() {
     fetch("https://fakestoreapi.com/products/categories")
       .then((response) => response.json())
       .then((data) => {
+        data.unshift("all")
         setCategories(data);
       });
   }
 
-  function LoadProducts() {
-    fetch("https://fakestoreapi.com/products")
+  function LoadProducts(url) {
+    fetch(url)
       .then((response) => response.json())
       .then((data) => {
         setProducts(data);
       });
   }
 
+  function handleCategoryChange(e){
+    if(e.target.value === 'all'){
+      LoadProducts("https://fakestoreapi.com/products");
+    }else{
+    LoadProducts(`https://fakestoreapi.com/products/category/${e.target.value}`)
+  }}
+
   useEffect(() => {
     LoadCategory();
-    LoadProducts();
-  }, []);
+    LoadProducts("https://fakestoreapi.com/products");
+  let results = products.filter((product) =>
+    product.title.toLowerCase().includes(search.toLowerCase())
+  );
+
+    setFilterProducts(results);
+  }, [search]);
+
+  const sortByPrice = (data) => {
+    let productsDataCopy;
+    if (data === "price") {
+      if(filterProducts.length > 0){
+        productsDataCopy = [...filterProducts];
+         productsDataCopy.sort((a, b) => {
+           return a.price - b.price;
+         });
+         setFilterProducts(productsDataCopy);
+      }else{
+        productsDataCopy = [...products];
+       productsDataCopy.sort((a, b) => {
+         return a.price - b.price;
+       });
+       setProducts(productsDataCopy);
+      }
+     
+    } else if (data === "rating") {
+       if(filterProducts.length > 0){
+        productsDataCopy = [...filterProducts];
+         productsDataCopy.sort((a, b) => {
+           return b.rating.rate - a.rating.rate;
+         });
+         setFilterProducts(productsDataCopy);
+        }else{
+          productsDataCopy = [...products];
+           productsDataCopy.sort((a, b) => {
+             return b.rating.rate - a.rating.rate;
+           });
+           setProducts(productsDataCopy);
+        }
+     
+    }
+  };
 
   return (
     <div className="App" style={{ fontFamily: "Arial, sans-serif" }}>
       <header
         style={{
-          textAlign: "center",
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
           padding: "20px",
-          backgroundColor: "#f4f4f4",
+          backgroundColor: "red",
         }}
       >
-        <h1>Shopping Home</h1>
+        <h1
+          style={{ flex: "1", textAlign: "start", color: "white", margin: "0" }}
+        >
+          Shopping Home
+        </h1>
+        <input
+          type="search"
+          placeholder="search here.."
+          value={search}
+          onChange={(e) => {
+            setSearch(e.target.value);
+          }}
+          style={{
+            flex: "2",
+            maxWidth: "300px",
+            padding: "10px",
+            borderRadius: "5px",
+            border: "1px solid #ccc",
+          }}
+        />
       </header>
+
       <section style={{ display: "flex", gap: "20px", padding: "20px" }}>
         <nav
           style={{
@@ -49,6 +121,7 @@ function App() {
             <label style={{ fontWeight: "bold" }}>Select Category</label>
             <div>
               <select
+                onChange={handleCategoryChange}
                 style={{
                   width: "100%",
                   padding: "10px",
@@ -57,9 +130,37 @@ function App() {
                 }}
               >
                 {categories.map((category) => (
-                  <option key={category}>{category}</option>
+                  <option value={category} key={category}>
+                    {category.toUpperCase()}
+                  </option>
                 ))}
               </select>
+            </div>
+            <div style={{ marginTop: "2rem" }}>
+              <button
+                style={{
+                  marginRight: "2rem",
+                  padding: "8px",
+                  backgroundColor: "smoke",
+                }}
+                onClick={() => {
+                  sortByPrice("price");
+                }}
+              >
+                Price
+              </button>
+              <button
+                style={{
+                  marginRight: "2rem",
+                  padding: "8px",
+                  backgroundColor: "smoke",
+                }}
+                onClick={() => {
+                  sortByPrice("rating");
+                }}
+              >
+                Rating
+              </button>
             </div>
           </div>
         </nav>
@@ -71,47 +172,89 @@ function App() {
             gridTemplateColumns: "repeat(auto-fill, minmax(200px, 1fr))",
           }}
         >
-          {products.map((product, index) => (
-            <div
-              key={index}
-              style={{
-                border: "1px solid #ddd",
-                borderRadius: "8px",
-                boxShadow: "0 2px 5px rgba(0,0,0,0.1)",
-                padding: "15px",
-                backgroundColor: "#fff",
-                textAlign: "center",
-              }}
-            >
-              <img
-                height="150" 
-                src={product.image}
-                alt=""
-                style={{ borderRadius: "8px", width: "100px"}}
-              />
-              <div
-                style={{
-                  margin: "10px 0",
-                  fontWeight: "bold",
-                  fontSize: "1rem",
-                }}
-              >
-                {product.title}
-              </div>
-              <div>
-                <dl style={{ margin: "0", textAlign: "left" }}>
-                  <dt style={{ fontWeight: "bold" }}>Price</dt>
-                  <dd style={{ margin: "0 0 10px 0" }}>
-                    ${product.price.toFixed(2)}
-                  </dd>
-                  <dt style={{ fontWeight: "bold" }}>Rating</dt>
-                  <dd style={{ margin: "0" }}>
-                    {product.rating.rate} ⭐ [{product.rating.count}]
-                  </dd>
-                </dl>
-              </div>
-            </div>
-          ))}
+          {filterProducts.length > 0
+            ? filterProducts.map((product, index) => (
+                <div
+                  key={index}
+                  style={{
+                    border: "1px solid #ddd",
+                    borderRadius: "8px",
+                    boxShadow: "0 2px 5px rgba(0,0,0,0.1)",
+                    padding: "15px",
+                    backgroundColor: "#fff",
+                    textAlign: "center",
+                  }}
+                >
+                  <img
+                    height="150"
+                    src={product.image}
+                    alt=""
+                    style={{ borderRadius: "8px", width: "100px" }}
+                  />
+                  <div
+                    style={{
+                      margin: "10px 0",
+                      fontWeight: "bold",
+                      fontSize: "1rem",
+                    }}
+                  >
+                    {product.title}
+                  </div>
+                  <div>
+                    <dl style={{ margin: "0", textAlign: "left" }}>
+                      <dt style={{ fontWeight: "bold" }}>Price</dt>
+                      <dd style={{ margin: "0 0 10px 0" }}>
+                        ${product.price.toFixed(2)}
+                      </dd>
+                      <dt style={{ fontWeight: "bold" }}>Rating</dt>
+                      <dd style={{ margin: "0" }}>
+                        {product.rating.rate} ⭐ [{product.rating.count}]
+                      </dd>
+                    </dl>
+                  </div>
+                </div>
+              ))
+            : products.map((product, index) => (
+                <div
+                  key={index}
+                  style={{
+                    border: "1px solid #ddd",
+                    borderRadius: "8px",
+                    boxShadow: "0 2px 5px rgba(0,0,0,0.1)",
+                    padding: "15px",
+                    backgroundColor: "#fff",
+                    textAlign: "center",
+                  }}
+                >
+                  <img
+                    height="150"
+                    src={product.image}
+                    alt=""
+                    style={{ borderRadius: "8px", width: "100px" }}
+                  />
+                  <div
+                    style={{
+                      margin: "10px 0",
+                      fontWeight: "bold",
+                      fontSize: "1rem",
+                    }}
+                  >
+                    {product.title}
+                  </div>
+                  <div>
+                    <dl style={{ margin: "0", textAlign: "left" }}>
+                      <dt style={{ fontWeight: "bold" }}>Price</dt>
+                      <dd style={{ margin: "0 0 10px 0" }}>
+                        ${product.price.toFixed(2)}
+                      </dd>
+                      <dt style={{ fontWeight: "bold" }}>Rating</dt>
+                      <dd style={{ margin: "0" }}>
+                        {product.rating.rate} ⭐ [{product.rating.count}]
+                      </dd>
+                    </dl>
+                  </div>
+                </div>
+              ))}
         </main>
       </section>
     </div>
